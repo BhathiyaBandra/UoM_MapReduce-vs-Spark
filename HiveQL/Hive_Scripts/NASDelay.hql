c2c892e1d4c0c63f0595e3f06fa6d7c8d3e72ac0
Bhathiya Bandara <bhathiya.bandara@outlook.com>
2023-03-03 17:28:39 +0530

    Initial Files

---
+++ HiveQL/Hive_Scripts/NASDelay.hql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS DelayedFlights;
+
+CREATE EXTERNAL TABLE DelayedFlights (
+    ID int,
+    Year int,
+    Month int,
+    DayofMonth int,
+    DayOfWeek int,
+    DepTime double,
+    CRSDepTime double,
+    ArrTime double,
+    CRSArrTime double,
+    UniqueCarrier string,
+    FlightNum double,
+    TailNum string,
+    ActualElapsedTime double,
+    CRSElapsedTime double,
+    AirTime double,
+    ArrDelay double,
+    DepDelay double,
+    Origin string,
+    Dest string,
+    Distance double,
+    TaxiIn int,
+    TaxiOut int,
+    Cancelled int,
+    CancellationCode string,
+    Diverted double,
+    CarrierDelay double,
+    WeatherDelay double,
+    NASDelay double,
+    SecurityDelay double,
+    LateAircraftDelay double
+	)
+	ROW FORMAT DELIMITED
+	FIELDS TERMINATED BY ','
+	LINES TERMINATED BY '\n'
+	STORED AS TEXTFILE
+	LOCATION "${INPUT}";
+
+ INSERT OVERWRITE DIRECTORY "${OUTPUT}"
+ SELECT Year, AVG(NASDelay) FROM DelayedFlights GROUP BY Year;
---
+++ Spark/Spark_Scripts/SecurityDelay.py
@@ -0,0 +1,26 @@
+import sys
+from operator import add
+from pyspark import SparkConf, SparkContext
+from pyspark.sql.functions import avg, col
+from pyspark.sql import SparkSession
+from pyspark.sql.types import ArrayType, IntegerType
+
+
+
+conf = (SparkConf().setAppName("SecurityDelay"))
+sc = SparkContext(conf = conf)
+spark = SparkSession(sc)
+
+headers = ["ID","Year","Month","DayofMonth","DayOfWeek","DepTime","CRSDepTime","ArrTime","CRSArrTime","UniqueCarrier","FlightNum","TailNum","ActualElapsedTime","CRSElapsedTime","AirTime","ArrDelay","DepDelay","Origin","Dest","Distance","TaxiIn","TaxiOut","Cancelled","CancellationCode","Diverted","CarrierDelay","WeatherDelay","NASDelay","SecurityDelay","LateAircraftDelay"]
+
+if __name__ == "__main__":
+    inputFile = "s3://bhathiya/input/DelayedFlights-updated.csv"
+    lines = sc.textFile(sys.argv[1])
+    lines_nonempty = lines.filter( lambda x: len(x) > 0 )
+    values = lines_nonempty.map(lambda line : line.split(","))
+    df1 = spark.createDataFrame(values,headers)
+    df2 = df1.withColumn("SecurityDelay",df1.SecurityDelay.cast('int'))
+    df2.groupBy("Year").avg("SecurityDelay").show(truncate=False)
+    sc.stop()
+    spark.stop()
+ 
